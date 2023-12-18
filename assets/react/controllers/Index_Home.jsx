ef6fd3e7a7c2ea37408e9be8d68e56f0b067aaf1
Benjamin-Sebert <benjaminsebert@skiff.com>
2023-12-18 15:01:53 +0100

    maj ben 2

--- assets/react/controllers/Index_Home.jsx
+++ assets/react/controllers/Index_Home.jsx
@@ -1,13 +1,13 @@
 // assets/react/App.js
 import React from 'react';
-import Sidebar from './Sidebar';
+import Navbar from './Navbar';
 import Footer from './Footer';
 import Dashboard from './Dashboard'; 
 
 const Index_Home = () => {
   return (
     <div className="flex flex-col">
-      <Sidebar />
+      <Navbar />
       <div className="bg-white">
       <Dashboard />
       </div>
