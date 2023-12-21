2af784c40f5cc18617b872fab3544ff4a7e292a4
Benjamin-Sebert <benjaminsebert@skiff.com>
2023-12-21 15:06:42 +0100

    api test

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     Symfony\UX\React\ReactBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,8 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
 use Symfony\UX\Chartjs\Model\Chart;
-
-use App\Entity\ContactMessage;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Article;
 
 class HomeController extends AbstractController
 {
@@ -26,5 +28,18 @@ class HomeController extends AbstractController
         return $this->render('home/ArticleGraph.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+    #[Route('/api/articles', name: 'app_apiarticles', methods: ['POST'])]
+    public function createArticle(Request $request, EntityManagerInterface $entityManager): Response
+    {
+
+        $article = new Article();
+        $article->setTitre($request->request->get('title'));
+        $article->setRésumé($request->request->get('summary'));
+
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return new Response('Article saved with id '.$article->getId());
     }
 }
