ee29bcf232899c14b1f3ef5d2b03167327d0dfdf
Benjamin-Sebert <benjaminsebert@skiff.com>
2023-12-21 09:33:21 +0100

    ben

--- assets/react/controllers/Article_Graph_Visualisation.jsx
+++ assets/react/controllers/Article_Graph_Visualisation.jsx
@@ -3,17 +3,18 @@ import GraphAffichage from './GraphAffichage';
 
 const ArticleClassiqueVisualisation = ({ article }) => {
     return (
-        <div className="w-full ml-4 border bg-white p-4 rounded">
-            <h2 className="text-2xl font-bold">{article.title}</h2>
-            <p className="text-gray-600 mb-4">{article.summary}</p>
+        <div className="w-full mx-auto p-4 bg-grey-200 shadow-md rounded-lg">
+
+            <h2 className="text-2xl font-bold break-words">{article.title}</h2>
+            <p className="text-gray-600 mb-4 break-words">{article.summary}</p>
 
             {article.blocks.map((block, index) => (
                 <div key={index} className="mb-4">
                     {block.type === 'title' && (
-                        <h3 className="text-xl font-semibold text-black">{block.content}</h3>
+                        <h3 className="text-xl font-semibold text-black break-words">{block.content}</h3>
                     )}
                     {block.type === 'text' && (
-                        <p className="mb-2 text-black">{block.content}</p>
+                        <p className="mb-2 text-black break-words">{block.content}</p>
                     )}
                     {block.type === 'image' && (
                         <img src={block.content} alt="Image" className="max-w-full" />
--- assets/react/controllers/Page_Article_Plus.jsx
+++ assets/react/controllers/Page_Article_Plus.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Dashboard_Graph from './Dashboard_Graph';
+import Article_Visualisation_Creation from './Article_Visualisation_Creation';
 
-const Sidebar = () => {
+const avcreation = () => {
   return (
     <div className="w-screen h-screen">
     <div className="flex flex-col md:flex-row h-screen">
-      <aside className="w-full md:w-1/12 bg-blue-600 p-4 shadow-xl md:shadow-none">
+      <aside className="w-full md:w-2/12 bg-blue-600 p-4 shadow-xl md:shadow-none">
         <ul className="space-y-2 text-white">
           <li><a href="#" className="block font-semibold hover:underline">Accueil</a></li>
           <li><a href="#" className="block font-semibold hover:underline">Backpacks</a></li>
@@ -19,13 +19,13 @@ const Sidebar = () => {
         <div className="flex items-center justify-between border-b border-gray-200 pb-4">
           <h1 className="text-3xl font-bold text-gray-800">SASCMS</h1>
           <div className="flex items-center space-x-4">
-            <button className="text-gray-600 hover:text-gray-800">Paramètres</button>
+            <button className="text-gray-600 hover:text-gray-800">Mon compte</button>
             <button className="text-gray-600 hover:text-gray-800">Deconnexion</button>
           </div>
         </div>
 
         <div className="gap-8 mt-6">
-          <Dashboard_Graph />
+          <Article_Visualisation_Creation />
         </div>
       </main>
     </div>
@@ -33,4 +33,4 @@ const Sidebar = () => {
   );
 };
 
-export default Sidebar;
+export default avcreation;
